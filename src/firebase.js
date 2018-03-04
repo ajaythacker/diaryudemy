da596245266b6e58099aa9a3c63ee145b8466523
ajaythacker <ajaythacker@gmail.com>
2018-03-03 23:45:55 -0700

    mod3 setup bootstrap and firebase complete

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+var config = {
+    apiKey: 'AIzaSyDPudjtV7mqOiAcdwpjsm56Kci0oCF10TU',
+    authDomain: 'diary-e138f.firebaseapp.com',
+    databaseURL: 'https://diary-e138f.firebaseio.com',
+    projectId: 'diary-e138f',
+    storageBucket: '',
+    messagingSenderId: '384973323231'
+};
+firebase.initializeApp(config);
+
+export const database = firebase.database().ref('/notes');
