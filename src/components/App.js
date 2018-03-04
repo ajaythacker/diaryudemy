510aff1605325b31f7bab43af8f833b1ad89f278
ajaythacker <ajaythacker@gmail.com>
2018-03-04 00:05:06 -0700

    mod5 added onchange and react local setstate

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+    state = {
+        title: '',
+        body: ''
+    };
+
+    handleChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value });
+    };
+
     render() {
         return (
             <div className='container-fluid'>
@@ -14,15 +23,17 @@ class App extends Component {
                                     className='form-control no-border'
                                     placeholder='Title...'
                                     required
+                                    onChange={this.handleChange}
                                 />
                             </div>
                             <div className='form-group'>
                                 <input
                                     type='text'
-                                    name='title'
+                                    name='body'
                                     className='form-control no-border'
                                     placeholder='Body...'
                                     required
+                                    onChange={this.handleChange}
                                 />
                             </div>
 
