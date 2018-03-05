0698911120492fc6d8a3ba42e2765ac3a2553c8a
Ajay Thacker <athacker@healthgrades.com>
2018-03-05 15:57:53 -0700

    mod21 fixed minor style break rule

--- src/components/Login.js
+++ src/components/Login.js
@@ -12,7 +12,7 @@ class Login extends Component {
           <div className="col-sm-6">
             <button className="btn btn-danger btn-lg">Login with Google</button>
           </div>
-
+          <br />
           <div className="col-sm-6">
             <button className="btn btn-success btn-lg">Login with Twitter</button>
           </div>
