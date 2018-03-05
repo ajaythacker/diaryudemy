8d383f51f84f4c8c9d9641241b443dc0af59c49f
ajaythacker <ajaythacker@gmail.com>
2018-03-04 22:44:27 -0700

    mod17 converted class component to functional or stateless or dumb component

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -1,11 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class NoteCard extends Component {
-    render() {
-        return (
-            <div className='jumbotron'>
-                <div>{this.props.children}</div>
-            </div>
-        );
-    }
-}
+const NoteCard = (props) => {
+    return (
+        <div className='jumbotron'>
+            <div>{props.children}</div>
+        </div>
+    );
+};
+
+export default NoteCard;
+
+//equivalent class component
+// export default class NoteCard extends Component {
+//     render() {
+//         return (
+//             <div className='jumbotron'>
+//                 <div>{this.props.children}</div>
+//             </div>
+//         );
+//     }
+// }
