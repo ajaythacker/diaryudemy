7cbf86e9f0aa6784b35ee2e3727c0f96c8badd59
ajaythacker <ajaythacker@gmail.com>
2018-03-04 21:44:37 -0700

    mod12 added redux actioncreators,actions,reducers,combineReducers,createStore,Provider

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// redux store -> reducers -> actions | applyMiddleware()
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+//provide the store to react
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 registerServiceWorker();
