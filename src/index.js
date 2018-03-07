0270fe71baec9f1c4ac66cb743ebf312cf461850
Ajay Thacker <athacker@healthgrades.com>
2018-03-07 14:58:23 -0700

    added comments to better understand react-router-dom

--- src/index.js
+++ src/index.js
@@ -3,12 +3,34 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import rootReducer from './reducers';
 import thunk from 'redux-thunk';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
+//BrowserRouter will mainly create a history object to keep track of the location (URL)
+//When location changes because of a navigation action, the child component (in this case LoadingComponent) will be re-rendered
+
+//Link is for the navigation menu , like the html anchor tag to define a hyperlink
+//Link is different than the anchor tag because they change the URL without refreshing the page
+
+//Route specifies the url that corresponds to each component
+
+//Switch component is used to render only the first route that matches the location
+
+//react-router-dom also contains a Redirect component which is used like a "default" in switch case where if none of the routes match inside Switch conmponent, it defaults
+//to the specified route :
+//Example:
+/*
+ <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/messages" component={Messages} />
+            <Route path="/about" component={About} />
+            <Redirect to="/" />
+          </Switch>
+*/
+
 import Login from './components/Login';
 import Header from './routes/Header';
 import LoadingComponent from './components/LoadingComponent';
@@ -22,22 +44,22 @@ const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk
 //provide the store to react
 
 ReactDOM.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <LoadingComponent>
-                <div>
-                    <Switch>
-                        <Route path='/login' component={Login} exact={true} />
-                        <AuthenticatedComponent>
-                            <Header />
-                            <Route path='/' component={App} exact={true} />
-                            <Route path='/:id' component={NoteDetail} exact={true} />
-                        </AuthenticatedComponent>
-                    </Switch>
-                </div>
-            </LoadingComponent>
-        </BrowserRouter>
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={store}>
+    <BrowserRouter>
+      <LoadingComponent>
+        <div>
+          <Switch>
+            <Route path="/login" component={Login} exact={true} />
+            <AuthenticatedComponent>
+              <Header />
+              <Route path="/" component={App} exact={true} />
+              <Route path="/:id" component={NoteDetail} exact={true} />
+            </AuthenticatedComponent>
+          </Switch>
+        </div>
+      </LoadingComponent>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
 );
 registerServiceWorker();
