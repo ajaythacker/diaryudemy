63c3acd0ffbbfcf6981f08ca1ccaa8fe2ddf688b
ajaythacker <ajaythacker@gmail.com>
2018-03-07 21:54:01 -0700

    38 note details from statetoprops

--- src/components/NoteDetail.js
+++ src/components/NoteDetail.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class NoteDetail extends Component {
     render() {
-        return <div>note detail...</div>;
+        const { note } = this.props;
+        return (
+            <div className='container-fluid'>
+                <div className='row'>
+                    <div className='col-sm-6 col-sm-offset-3'>
+                        <h1>{note.title}</h1>
+                        <p>{note.body}</p>
+                        <Link to='/'>Back</Link>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
-export default connect()(NoteDetail);
+
+const mapStateToProps = (state, ownProps) => {
+    return {
+        note: state.notes[ownProps.match.params.id]
+    };
+};
+
+export default connect(mapStateToProps)(NoteDetail);
